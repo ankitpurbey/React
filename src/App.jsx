6652d2bdc14ad04b1360e87f75083d23fba1d571
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-06-23 21:10:08 +0530

    card

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import React from "react"
-import Navbar from "./components/Navbar";
-import SecondPage from "./components/SecondPage";
+import Navbar from "./components/Navbar"
+
+
 function App(){
   return(
     <div>
       <Navbar />
-      <SecondPage />
       <h2>Hello, World!</h2>
     </div>
-    
-
   )
 }
+
 export default App;
---
+++ src/components/card1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const card1 = () => {
+  return (
+    <div className='w-full h-screen bg-zinc-50'>
+      <div className='flex justify-center items-center h-full'></div>
+    </div>
+  )
+}
+
+export default card1
