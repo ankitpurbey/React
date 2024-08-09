3142af9a3ce8411354064c1b42ce28e840e8bdde
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-08-10 01:42:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,13 @@
  
  
  const App = () => {
+
+  const data =["Ankit","Aman", "Amit", "Ankit kumar"]
    return (
      <div>
-       <div className=" px-3 py-4 bg-zinc-200  rounded-md w-fit ml-3 mt-2 " > hey</div>
+      
+         {data.map((elem , index)=>(<div className=" px-3 py-4 bg-zinc-200  rounded-md w-fit ml-3 mt-2 " > 
+          </div>))}
      </div>
    )
  }
