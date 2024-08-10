ad4ce6a895693e4ba4ecb58959aa345c7813d2f1
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-08-10 17:27:10 +0530

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,7 @@
   const data =["Ankit","Aman", "Amit", "Ankit kumar"]
    return (
      <div>
-      
-         {data.map((elem , index)=>(<div className=" px-3 py-4 bg-zinc-200  rounded-md w-fit ml-3 mt-2 " > 
-          </div>))}
+      <SecondPage/>
      </div>
    )
  }
--- src/components/SecondPage.jsx
+++ src/components/SecondPage.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const SecondPage = () => {
-  return (
-    <div className='w-full  h-screen bg-zinc-200'>
-      <div className=' px-4  py-4 bg bg-zinc-200 absolute left-1/2  top-1/2'></div>
+function SecondPage(){
+   return(
+    <div className=' w-full h-screen  bg-zinc-200'>
+      <div className=' w-52 bg-zinc-100 absolute left-1/2 top-1/2 translate-x-y'></div>
     </div>
-    )
+   )
 }
 
 export default SecondPage
