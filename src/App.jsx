ea31084dce8e4d36400276e1796ed08144066677
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-09-09 19:58:39 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
- import React from 'react'
+ import React, { useState } from 'react'
 import Props from './components/Props'
  
  export default function App() {
+  const data =[
+    {name:"Ankit" , proffession:"WebDeveloper",image:"https://images.unsplash.com/photo-1519058082700-08a0b56da9b4?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fG1lbnxlbnwwfHwwfHx8MA%3D%3D"},
+    {name:"Mohan" , proffession:"App Developer",image:"https://images.unsplash.com/photo-1496346236646-50e985b31ea4?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bWVufGVufDB8fDB8fHww"},
+    {name:"Nilesh" , proffession:"Salse",image:"https://plus.unsplash.com/premium_photo-1672239496290-5061cfee7ebb?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bWVufGVufDB8fDB8fHww"},
+    {name:"Monkesj" , proffession:"BlockChain",image:"https://plus.unsplash.com/premium_photo-1661400600986-b7493240cae2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8bWVufGVufDB8fDB8fHww"},
+  ]
+
+  const [val,SetVal] = useState(data);
+
+  const handleCkick =(changeIndex)=>{
+    SetVal((prev)=>{
+      return prev.map((item,index)=>{
+        if(index === changeIndex){
+          return {...item, isLiked:!item.isLiked}
+        }
+        return item;
+      })
+    })
+  }
+  const dummy =()=> alert("Request Sent");
    return (
      <div>
-       <Props text ="Click Here" color =" bg-blue-500"/>
-       <Props text ="Download" color =" bg-red-400"/>
-       <Props text ="New Folder" color  =" bg-yellow-300"/>
-
+       <div className=' w-full h-screen bg-zinc-400 flex gap-5 items-center justify-center'>
+        {val.map((item, i) =>(
+           <Props image ={item.image} dummy ={dummy} name ={ item.name} proffession ={ item.proffession}/>
+        ))}
+       </div>
      </div>
    )
  }
--- src/components/Props.jsx
+++ src/components/Props.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
-export default function Props({text,color}) {
+export default function Props({image,name,proffession} , dummy) {
   return (
-    <button className={`px-3 py-2 ${color} rounded-full text-xs ml-3 mt-5`}>
-        {text} </button>
+    <div className=' w-52 bg-white ml-2 mt-2'>
+      <div className=' w-full h-35 bg-sky-300 items-center '>
+        <img className=' w-full h-full object-cover ' src={image} alt="" />
+      </div>
+      <div className=' w-full p-3'>
+        <h3 className=' text-xs items-center font-semibold'> {name}</h3>
+        <h4 className=' text-xs '>{proffession}</h4>
+        <button onClick={()=> handleCkick( index)} className=' px-2 py-1 bg-green-400 text-xs rounded-md mt-2'>Send Request</button>
+      </div>
+    </div>
   )
 }
