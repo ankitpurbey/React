648d882dfd3fc168686334aa243bc73a6f2583a4
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-09-17 00:14:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ import Cards from './Cards'
    return (
      <nav className=' items-center flex'>
       <Link className=' ml-2 mr-3 text-cyan-400' to ="/home">home</Link>
-      <Link className=' ml-2 mr-3 text-black' to ="/Peoject">Project</Link>
-      <Link className=' ml-2 mr-3' to ="/New">home</Link>
-      <Link className=' ml-2 mr-3' to ="/home">home</Link>
+      <Link className=' ml-2 mr-3 text-black' to ="/Project">Project</Link>
+      <Link className=' ml-2 mr-3' to ="/User">User</Link>
+      <Link className=' ml-2 mr-3' to ="/About">About</Link>
      </nav>
    )
  }
