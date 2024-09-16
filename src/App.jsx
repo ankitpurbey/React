436a34bcc0592302cb825d68374adb0d13579f9e
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-09-17 00:04:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
  import React, { useState } from 'react'
-import FormHandlingEXE from './FormHandlingEXE'
+import {Link} from "react-router-dom";
 import Cards from './Cards'
  
  export default function App() {
-  const [user ,setUser] = useState([{name:"Ankit"}])
+  
    return (
-     <div className=' h-screen w-full p-10 bg-zinc-200 flex items-center justify-center'>
-      <div className =' container mx-auto' >
-        <Cards user ={user}/>
-        <FormHandlingEXE/>
-      </div>
-     </div>
+     <nav className=' items-center flex'>
+      <Link className=' ml-2 mr-3 text-cyan-400' to ="/home">home</Link>
+      <Link className=' ml-2 mr-3 text-black' to ="/Peoject">Project</Link>
+      <Link className=' ml-2 mr-3' to ="/New">home</Link>
+      <Link className=' ml-2 mr-3' to ="/home">home</Link>
+     </nav>
    )
  }
  
